a31df29a1262779653a1cbf0f6c3ffd3a176249f
fiany <f848021126@outlook.com>
2019-07-17 14:09:31 +0800

    添加配置nacos

--- src/main/java/ink/fiany/learnjava/LearnjavaApplication.java
+++ src/main/java/ink/fiany/learnjava/LearnjavaApplication.java
@@ -3,7 +3,9 @@ package ink.fiany.learnjava;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("ink.fiany.learnjava.dao")
 @SpringBootApplication
 public class LearnjavaApplication {
